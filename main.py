a00ecfd2ff5c0041653e85498e1d487020d9c4c2
Bryce Catlin <bryce@shipt.com>
2020-09-04 20:36:21 -0700

    Parse IP from discover, extract Dance function

--- main.py
+++ main.py
@@ -4,34 +4,40 @@ from yeelight import discover_bulbs
 from yeelight import Bulb
 
 
+# Find bulb
 print("Discovering bulbs...")
-mybulb = discover_bulbs()
-print(json.dumps(mybulb, indent=2, sort_keys=False))
-print("Done")
+bulbs = discover_bulbs()
+print("Found bulb:")
+print(json.dumps(bulbs, indent=2, sort_keys=False))
+ip = bulbs[0]["ip"]
+print("Bulb IP address: ", ip)
+bulb = Bulb(ip)
+print("")
 
-bulb = Bulb("192.168.23.224")
-bulb.turn_off()
-print("Ready - go check me out!")
-time.sleep(10)
-print("Starting...")
-bulb.turn_on()
-bulb.set_brightness(50)
+def dance(bulb):
+    bulb.turn_off()
+    print("Ready to dance - go check me out!")
+    time.sleep(10)
+    print("Dancing...")
+    bulb.turn_on()
+    bulb.set_brightness(50)
 
-base = 16
-multiple = (256/base)-1
+    base = 16
+    multiple = (256/base)-1
 
-for i in range(1,2):
-    print("Cycle ", i, "...")
-    for j in range(0,multiple):
-        bulb.set_rgb(base*(multiple-j), base*(j), 0)
-        time.sleep(.25)
-    for j in range(0,multiple):
-        bulb.set_rgb(0, base*(multiple-j), base*(j))
-        time.sleep(.25)
-    for j in range(0,multiple):
-        bulb.set_rgb(base*(j), 0, base*(multiple-j))
-        time.sleep(.25)
+    for i in range(1,2):
+        print("Cycle ", i, "...")
+        for j in range(0,multiple):
+            bulb.set_rgb(base*(multiple-j), base*(j), 0)
+            time.sleep(.25)
+        for j in range(0,multiple):
+            bulb.set_rgb(0, base*(multiple-j), base*(j))
+            time.sleep(.25)
+        for j in range(0,multiple):
+            bulb.set_rgb(base*(j), 0, base*(multiple-j))
+            time.sleep(.25)
+    print("Cycle done, turning off")
+    bulb.turn_off()
 
+dance(bulb)
 
-print("Cycle done, turning off")
-bulb.turn_off()

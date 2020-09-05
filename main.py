7bca7811f44458ccd0e6e35198748570315e3eb9
Bryce Catlin <bryce@shipt.com>
2020-09-04 21:59:02 -0700

    Set scene functions

--- main.py
+++ main.py
@@ -4,23 +4,9 @@ from yeelight import discover_bulbs
 from yeelight import Bulb
 
 
-# Find bulb
-print("Discovering bulbs...")
-bulbs = discover_bulbs()
-print("Found bulb:")
-print(json.dumps(bulbs, indent=2, sort_keys=False))
-ip = bulbs[0]["ip"]
-print("Bulb IP address: ", ip)
-bulb = Bulb(ip)
-print("")
-
+# Functions
 def dance(bulb):
-    bulb.turn_off()
-    print("Ready to dance - go check me out!")
-    time.sleep(10)
     print("Dancing...")
-    bulb.turn_on()
-    bulb.set_brightness(50)
 
     base = 16
     multiple = (256/base)-1
@@ -36,8 +22,61 @@ def dance(bulb):
         for j in range(0,multiple):
             bulb.set_rgb(base*(j), 0, base*(multiple-j))
             time.sleep(.25)
-    print("Cycle done, turning off")
-    bulb.turn_off()
+    print("Cycle done")
 
-dance(bulb)
 
+def setFree(bulb):
+    bulb.set_rgb(0, 255, 13)
+    print("Set bulb to \"Free\"")
+
+def setMeeting(bulb):
+    bulb.set_rgb(5, 65, 255)
+    print("Set bulb to \"Meeting\")
+
+def setDND(bulb):
+    bulb.set_rgb(255, 31, 2)
+    print("Set bulb to \"Do not disturb\"")
+
+def printColor(blob):
+    rgb = int(blob[0]["capabilities"]["rgb"])
+    red = (rgb >> 16) & 0x00ff
+    green = (rgb >> 8) & 0x00ff
+    blue = rgb & 0x00ff
+    #s = 'RGB (' + str(red) + ', ' + str(green) + ', ' + str(blue) + ')'
+    #print s
+    print("RGB (", red, ", ", green, ", ", blue, ")")
+
+
+# MAIN
+
+# Find bulb
+print("Discovering bulbs...")
+bulbs = discover_bulbs()
+print("Found bulb:")
+print(json.dumps(bulbs, indent=2, sort_keys=False))
+ip = bulbs[0]["ip"]
+#s = 'Bulb IP address: ' + ip
+#print s
+print()
+print("'Bulb IP address: ", ip)
+#printColor(bulbs)
+
+
+# Get Ready
+bulb = Bulb(ip)
+bulb.turn_on()
+bulb.set_brightness(50)
+print("Ready - go check me out!")
+time.sleep(10)
+
+# Act
+#dance(bulb)
+setFree(bulb)
+time.sleep(5)
+setMeeting(bulb)
+time.sleep(5)
+setDND(bulb)
+time.sleep(5)
+
+# Turn off
+bulb.turn_off()
